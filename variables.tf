3cc91187c5c0619990cf1ac3c10077db4ba54583
Oded Rafi <Odedrafi74@gmail.com>
2022-03-25 23:11:52 +0300

    Addind var file and making standard var file for name and addres prefixes

---
+++ variables.tf
@@ -0,0 +1,33 @@
+
+/*
+
+STANDARD VERIABLES TO USE IN OUR CODE 
+
+
+*/
+
+
+variable "prefix" {
+  type = map(string)
+  default = {
+    VnetName         = "HighAvailability"
+    AppServerName1   = "AppServer1"
+    AppServerName2   = "AppServer2"
+    AppServerName3   = "AppServer3"
+    PgDataServerName = "PgDataServer"
+
+  }
+}
+
+variable "address_space" {
+  type    = list(any)
+  default = ["10.30.0.0/16"]
+}
+
+
+variable "location" {
+  type        = string
+  description = "Azure location of terraform server environment"
+  default     = "East US"
+
+}
