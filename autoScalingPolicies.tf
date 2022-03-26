d68638039b24acbd45fa22979ac524383291fbce
Oded Rafi <Odedrafi74@gmail.com>
2022-03-26 21:05:44 +0300

    adding auto scale policy

---
+++ autoScalingPolicies.tf
@@ -0,0 +1,70 @@
+resource "azurerm_monitor_autoscale_setting" "AutoScaling" {
+  name                = "AutoScaling"
+  resource_group_name = azurerm_resource_group.RG.name
+  location            = azurerm_resource_group.RG.location
+  target_resource_id  = azurerm_linux_virtual_machine_scale_set.AppScaleSet.id
+
+  profile {
+    name = "defaultProfile"
+
+    capacity {
+      default = 2
+      minimum = 1
+      maximum = 3
+    }
+
+    rule {
+      metric_trigger {
+        metric_name        = "Percentage CPU"
+        metric_resource_id = azurerm_linux_virtual_machine_scale_set.AppScaleSet.id
+        time_grain         = "PT1M"
+        statistic          = "Average"
+        time_window        = "PT5M"
+        time_aggregation   = "Average"
+        operator           = "GreaterThan"
+        threshold          = 75
+        metric_namespace   = "linux.compute/virtualmachinescalesets"
+        dimensions {
+          name     = "AppName"
+          operator = "Equals"
+          values   = ["App1"]
+        }
+      }
+
+      scale_action {
+        direction = "Increase"
+        type      = "ChangeCount"
+        value     = "1"
+        cooldown  = "PT1M"
+      }
+    }
+
+    rule {
+      metric_trigger {
+        metric_name        = "Percentage CPU"
+        metric_resource_id = azurerm_linux_virtual_machine_scale_set.AppScaleSet.id
+        time_grain         = "PT1M"
+        statistic          = "Average"
+        time_window        = "PT5M"
+        time_aggregation   = "Average"
+        operator           = "LessThan"
+        threshold          = 25
+      }
+
+      scale_action {
+        direction = "Decrease"
+        type      = "ChangeCount"
+        value     = "1"
+        cooldown  = "PT1M"
+      }
+    }
+  }
+
+#   notification {
+#     email {
+#       send_to_subscription_administrator    = true
+#       send_to_subscription_co_administrator = true
+#       custom_emails                         = ["admin@contoso.com"]
+#     }
+#   }
+}
