5db93d1493a9da0fc37024ae03bc2f0ca9fb35c2
Oded Rafi <Odedrafi74@gmail.com>
2022-03-30 21:29:15 +0300

    adding ssh connection

--- NSGs.tf
+++ NSGs.tf
@@ -15,21 +15,29 @@ resource "azurerm_network_security_group" "NSG1" {
   name                = "NSG1"
   location            = azurerm_resource_group.RG.location
   resource_group_name = azurerm_resource_group.RG.name
+  security_rule {
+    name                       = "ssh"
+    priority                   = 110
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = "79.178.9.59"
+    destination_address_prefix = "*"
+  }
+  security_rule {
+    name                       = "Allow_8080"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "8080"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
 
-
-}
-resource "azurerm_network_security_rule" "Allow_8080" {
-  name                        = "Allow_8080"
-  priority                    = 100
-  direction                   = "Inbound"
-  access                      = "Allow"
-  protocol                    = "Tcp"
-  source_port_range           = "*"
-  destination_port_range      = "8080"
-  source_address_prefix       = "*"
-  destination_address_prefix  = "*"
-  resource_group_name         = azurerm_resource_group.RG.name
-  network_security_group_name = azurerm_network_security_group.NSG1.name
 }
 
 /*----------------------------------------------------------------------------------------*/
@@ -41,7 +49,7 @@ resource "azurerm_network_security_rule" "Allow_8080" {
 resource "azurerm_subnet_network_security_group_association" "NSG2_association" {
   subnet_id                 = azurerm_subnet.Data_Tier.id
   network_security_group_id = azurerm_network_security_group.NSG2.id
-   
+
 
 }
 /* BECAUSE WE WANT THE DATA TIER TO REMAINE "HIDDEN" TO OUTSIDE EYES WE CAN LEAVE THE STANDARD  */
@@ -50,8 +58,8 @@ resource "azurerm_network_security_group" "NSG2" {
   name                = "NSG2"
   location            = azurerm_resource_group.RG.location
   resource_group_name = azurerm_resource_group.RG.name
-    
-    security_rule {
+
+  security_rule {
     name                       = "Postgres"
     priority                   = 110
     direction                  = "Inbound"
