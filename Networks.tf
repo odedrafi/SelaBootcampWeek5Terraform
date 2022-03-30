fc4c4fc328134c289bf18f6386119616a82abe69
Oded Rafi <Odedrafi74@gmail.com>
2022-03-30 21:26:10 +0300

    moving all network reasources for more readable code

---
+++ Networks.tf
@@ -0,0 +1,46 @@
+
+/*----------------------------------------------------------------------------------------*/
+# Create a virtual network
+/*----------------------------------------------------------------------------------------*/
+resource "azurerm_virtual_network" "vnet" {
+  name                = "${var.prefix.VnetName}-Net"
+  address_space       = var.address_space
+  location            = azurerm_resource_group.RG.location
+  resource_group_name = azurerm_resource_group.RG.name
+}
+/*----------------------------------------------------------------------------------------*/
+
+/*----------------------------------------------------------------------------------------*/
+# Creat a subnet for the data base
+/*----------------------------------------------------------------------------------------*/
+resource "azurerm_subnet" "Data_Tier" {
+  name                 = "Data_Tier"
+  resource_group_name  = azurerm_resource_group.RG.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.30.2.0/24"]
+  # enforce_private_link_endpoint_network_policies = true
+  service_endpoints = ["Microsoft.Storage"]
+  delegation {
+    name = "fs"
+    service_delegation {
+      name = "Microsoft.DBforPostgreSQL/flexibleServers"
+      actions = [
+        "Microsoft.Network/virtualNetworks/subnets/join/action",
+      ]
+    }
+  }
+
+}
+
+/*----------------------------------------------------------------------------------------*/
+# Creat a subnet for the app servers the web tier
+/*----------------------------------------------------------------------------------------*/
+resource "azurerm_subnet" "Web_Tier" {
+  name                 = "Web_Tier"
+  resource_group_name  = azurerm_resource_group.RG.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.30.1.0/24"]
+
+
+}
+/*----------------------------------------------------------------------------------------*/
